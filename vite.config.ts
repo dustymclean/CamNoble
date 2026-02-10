480e6ad82b6890d45aea774d9477613deb8395b2
Dusty <168149925+dustymclean@users.noreply.github.com>
2026-02-10 09:43:45 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/CamNoble/', // <--- ADD THIS LINE
       server: {
         port: 3000,
         host: '0.0.0.0',
